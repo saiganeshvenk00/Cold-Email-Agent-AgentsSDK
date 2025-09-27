175caf5559c34b287f85ea18b9e88dc422635af8
saiganeshvenk00 <saiganeshvenkataraman@gmail.com>
2025-09-26 20:00:46 -0500

    Skeletal repo and code

---
+++ cold_pipeline/__init__.py
@@ -0,0 +1,3 @@
+"""Cold outreach pipeline package (flattened)."""
+from .workflow import run_cold_workflow
+from .agents import sales_manager, email_manager, sales_picker
---
+++ reply_pipeline/__init__.py
@@ -0,0 +1,2 @@
+from .workflow import run_reply_workflow
+from .agents import reply_agent, reply_email_manager
